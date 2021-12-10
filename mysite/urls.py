02f322f706a4edf3329d87e0e790fd9efa6ac738
ahmedYasser32 <ahmed.yasser32@gmail.com>
2021-12-10 14:45:56 +0200

    #

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', include('userlogin.urls')),
+    path('accounts/', include('account.urls')),
 
 ]
