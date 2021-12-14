be471e0c32f5bbb4c0bf058a185274854c4e58b5
MohamedAlsony <mohamedahmedelsony@gmail.com>
2021-12-14 20:56:26 +0200

    solving token error

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,8 +22,6 @@ from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    path('accounts/', include('account.urls')),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    #path('accounts/', include('account.urls')),
 ]
 
