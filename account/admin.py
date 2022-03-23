2c96e03711e01ee188947c66a546edd2db67c17d
MohamedAlsony <mohamedahmedelsony@gmail.com>
2022-03-23 19:03:04 +0200

    enhance account admin

--- account/admin.py
+++ account/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from account.models import Account,Profile
 
 # Register your models here.
-admin.site.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('email','firstname','lastname', 'is_company','date_joined', 'last_login', 'file',)
+    search_fields = ('pk', 'email','firstname', 'lastname')
+    readonly_fields=('pk', 'date_joined', 'last_login')
+
+    filter_horizontal = ()
+    list_filter = ('is_company',)
+    fieldsets = ()
+
+
+admin.site.register(Account, AccountAdmin)
 admin.site.register(Profile)
