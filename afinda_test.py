2b7f96b95eb82a89814e9b0e1b5a8565756d3c59
ahmedYasser32 <ahmed.yasser32@gmail.com>
2022-03-26 17:34:35 +0200

    affinda tests final

--- afinda_test.py
+++ afinda_test.py
@@ -20,16 +20,18 @@ client = AffindaAPI(credential=credential)
 # all_resumes = client.get_all_resumes()
 #pprint(all_resumes.as_dict())
 #mo7amed cv
-identifier = 'zuDqhDNZ'
+#identifier = 'zuDqhDNZ'
 
 #sonni cv
 #identifier = 'neuGGjlM'
 #ahmad identifier
 #identifier= 'mqfFjAWv'
 #aly cv
-identifier='HygHyXbd'
+#identifier='HygHyXbd'
 #Marwancv
 #identifier= 'IYWDMHYX'
+#AHMAD LAST CV
+identifier='QbemXfNR'
 
 resume = client.get_resume(identifier=identifier)
 
@@ -40,6 +42,7 @@ resume=resume.as_dict()
 pprint(resume['data'].keys())
 pprint(resume['meta'])
 pprint(resume)
+
 x=0
 
 print(f"{resume['data']['is_resume_probability']}%")
@@ -53,12 +56,14 @@ if 'name' in resume['data']:
     x+=2
 
 if  'phone_numbers' in resume['data']:
+
     if resume['data']['phone_numbers']:
         phonenumber = resume['data']['phone_numbers'][0]
         print(f"Phone number :{phonenumber}")
         x+=1
 
 if 'emails' in resume['data']:
+
     if resume['data']['emails']:
         email = resume['data']['emails'][0]
         print(f"email is {email}")
@@ -79,53 +84,97 @@ if 'skills' in resume['data']:
     #print(resume['data']['skills'])
     skills_table = pd.DataFrame.from_dict(resume['data']['skills'])
     #print(skills_table)
-    skills_list=skills_table.name.values
+    skills_list = skills_table.name.values
     skills=', '.join(map(str, skills_list))
     print(f"Skills is {skills}")
     x+=1
 
 if 'education' in resume['data']:
-    if 'organization' in resume['data']['education'][0]:
-        University     = resume['data']['education'][0]['organization']
-        print(f" University :{University} ")
-        x+=1
-    if 'accreditation' in resume['data']['education'][0]:
-         if 'education_level' in resume['data']['education']:
-             education_level    = resume['data']['education'][0]['accreditation']['education']
-             print(f"education level:{education_level}")
-             x+=1
-             if 'grade' in resume['data']['education'][1]['accreditation'] :
-                  gpa = resume['data']['education'][1]['accreditation']['grade']['value']
-                  print(f"Gpa : {gpa}")
-                  x+=1
+
+    organization=[]
+    edlevel=[]
+    gpas=[]
+    year_of_grad=[]
+    fieldofStudy=[]
+
+    if resume['data']['education']:
+
+        for i in range(len(resume['data']['education'])):
 
 
-    if 'dates' in resume['data']['education'][0]:
-     year_of_graduation = resume['data']['education'][0]['dates']['completion_date'].split('-')[0]
-     print(f"Year of graduation:{year_of_graduation}")
-     x+=1
+            if 'organization' in resume['data']['education'][i]:
+                University     = resume['data']['education'][i]['organization']
+
+                organization.append(University)
+                Universities=', '.join(organization)
+                print(f" University :{Universities} ")
+                x+=1
+
+            if 'accreditation' in resume['data']['education'][i]:
+
+                if 'education_level' in resume['data']['education'][i]['accreditation']:
+                        education_level    = resume['data']['education'][i]['accreditation']['education_level']
+                        edlevel.append(education_level)
+                        edLevel=', '.join(edlevel)
+                        x+=1
+                        print(f"Education_level :{edLevel}")
+
+                if 'education' in resume['data']['education'][i]['accreditation']:
+                        education    = resume['data']['education'][i]['accreditation']['education']
+
+                        fieldofStudy.append(education)
+                        StudyFields=', '.join(fieldofStudy)
+                        x+=1
+                        print(f"Study field:{StudyFields}")
+
+
+                if 'grade' in resume['data']['education'][i] :
+                    gpa = resume['data']['education'][i]['grade']['value']
+                    print("gpa loop")
+
+                    gpas.append(gpa)
+                    GPA=', '.join(gpas)
+                    print(f"Print Gpa is: {GPA}")
+                    x+=1
+
+
+                if 'dates' in resume['data']['education'][i]:
+                    year = resume['data']['education'][i]['dates']['completion_date'].split('-')[i]
+                    year_of_grad.append(year)
+                    year_of_graduation=max(year_of_grad)
+                    print(f"Year of grad is :{year_of_graduation}")
+                    x+=1
+
 
 if 'location' in resume['data']:
+
     if 'country' in resume['data']['location']:
      country = resume['data']['location']['country']
      print(f"Country is : {country}")
      x+=1
+
     if'state' in resume['data']['location']:
      city    = resume['data']['location']['state']
      print(f"City is :{city}")
      x+=1
+
     area=resume['data']['location']['raw_input']
     print(f"adress :{area}")
     x+=1
 
 if'work_experience' in  resume['data']:
-    job_title=[]
-    if resume['data']['work_experience']:
-     x+=1
-     for i in range(len( resume['data']['work_experience'])):
-         job_title = resume['data']['work_experience'][i]['job_title']
 
-         print(f"Job Title intrested in: {job_title}")
+    if resume['data']['work_experience']:
+        length =  len( resume['data']['work_experience'])
+        job_title=[]
+
+        x+=1
+
+        for i in range(length):
+
+            job_title.append(resume['data']['work_experience'][i]['job_title'])
+            jobs_intrested =', '.join(job_title)
+            print(f"Job Title intrested in: {jobs_intrested}")
 
 if 'websites' in resume['data']:
     if resume['data']['websites']:
