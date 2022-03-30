f48d9fc9816d9817e8982cd47c957fd87e434265
ahmedYasser32 <ahmed.yasser32@gmail.com>
2022-03-30 17:28:24 +0200

    jobs model with migrations

--- jobs/models.py
+++ jobs/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from companies.models import CompanyProfile
+from account.models import Profile
 
 class Jobs(models.Model):
 
-    company                 = models.ForeignKey(CompanyProfile,on_delete=models.CASCADE)
-    education_level_choices = [("ST","Student"),("BCH","Bachelor"),("UG","UnderGrad"),("MST","Masters"),("PHD","PHD")]
-    description             = models.CharField(max_length=1000)
-    requirements            = models.CharField(max_length=1000)
-    education_level         = models.CharField(max_length=15,choices=education_level_choices,default="BCH")
-    experience              = models.CharField(max_length=2)
-    salary                  = models.CharField(max_length=10)
-    isConfidential          = models.BooleanField(default=True)
-    #applicant               = models.
-
+    company                 =  models.ForeignKey(CompanyProfile,on_delete=models.CASCADE)
+    education_level_choices =  [("ST","Student"),("BCH","Bachelor"),("UG","UnderGrad"),("MST","Masters"),("PHD","PHD")]
+    description             =  models.CharField(max_length=1000)
+    requirements            =  models.CharField(max_length=1000)
+    education_level         =  models.CharField(max_length=15,choices=education_level_choices,default="BCH")
+    experience              =  models.CharField(max_length=2)
+    salary                  =  models.CharField(max_length=10)
+    isConfidential          =  models.BooleanField(default=True)
+    applicants              =  models.ManyToManyField(Profile)
 
