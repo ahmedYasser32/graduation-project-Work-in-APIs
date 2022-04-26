ee1463c1566a176585757f99b06ddd1efb433351
Ahmad-sonni <mohamedahmedelsony@gmail.com>
2022-04-26 05:24:25 +0200

    '#'

--- mysite/settings.py
+++ mysite/settings.py
@@ -177,7 +177,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'master.clean.dcl@gmail.com'
-EMAIL_HOST_PASSWORD = 'mgomkeevymgtfuxz'
+EMAIL_HOST_PASSWORD = 'jfijlqmpzdzpsrvr'
 EMAIL_PORT = 587
 
 # Default primary key field type
